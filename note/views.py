bec93a8634d000d375f13aed677ad40747216667
woong <dndvud123@naver.com>
2022-06-22 22:40:59 +0900

    append list.html, post_list views

--- note/views.py
+++ note/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
+from .models import Post
 
 # Create your views here.
 def index(request):
     return render(request, 'note/index.html')
+
+def post_list(request):
+    context = {}
+    posts = Post.objects.all()
+    context["posts"] = posts
+    
+    return render(request, "note/page_list.html", context=context)
