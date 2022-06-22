83761601a695a5e1824eb1ab4681c1308cf9e465
woong <dndvud123@naver.com>
2022-06-23 00:35:02 +0900

    modified urls, append detail.html, info.html, add views.py functions

--- note/urls.py
+++ note/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index),
-    path('list/', views.post_list)
+    path('', views.index, name="index"),
+    path('info/', views.info, name="info"),
+    path('list/', views.post_list, name='post-list'),
+    path('post/<int:post_id>/', views.post_detail, name='post-detail'),
 ]
--- note/views.py
+++ note/views.py
@@ -5,9 +5,18 @@ from .models import Post
 def index(request):
     return render(request, 'note/index.html')
 
+def info(request):
+    return render(request, 'note/info.html')
+
 def post_list(request):
     context = {}
     posts = Post.objects.all()
     context["posts"] = posts
     
     return render(request, "note/page_list.html", context=context)
+
+def post_detail(request, post_id):
+    context = {}
+    post_id = Post.objects.get(id=post_id)
+    context["post"] = post_id
+    return render(request, 'note/page_detail.html', context=context)
