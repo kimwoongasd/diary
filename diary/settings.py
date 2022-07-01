9a1839d9476d3c1dca136a1772b9e4e85ea93aef
woong <dndvud123@naver.com>
2022-07-01 21:22:04 +0900

    append singup, login css, and message, email overriding

--- diary/settings.py
+++ diary/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'django.contrib.sites',
     'note',
+    'widget_tweaks',
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
