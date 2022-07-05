8f2412457671346c90b6feb872924bdfab354454
woong <dndvud123@naver.com>
2022-07-06 02:05:04 +0900

    append profile_pic

--- note/admin.py
+++ note/admin.py
@@ -4,4 +4,4 @@ from .models import Post, User
 # Register your models here.
 admin.site.register(User, UserAdmin)
 admin.site.register(Post)
-UserAdmin.fieldsets += (("Custom fields", {"fields": ("nickname",)}), )
+UserAdmin.fieldsets += (("Custom fields", {"fields": ("nickname", "profile_pic")}), )
--- note/models.py
+++ note/models.py
@@ -1,3 +1,4 @@
+from email.policy import default
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 from .validators import validate_no_numbers, validate_score, validate_no_special_characters
@@ -11,6 +12,8 @@ class User(AbstractUser):
         validators=[validate_no_special_characters],
         error_messages={"unique": "이미 사용중인 닉네임입니다."},
         )
+    
+    profile_pic = models.ImageField(default="default_profile_pic.jpg", upload_to="profile_pics")
     
     def __str__(self):
         return self.email
