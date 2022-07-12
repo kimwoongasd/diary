28c92810924e6d01f5132841539d4b22ec10455b
woong <dndvud123@naver.com>
2022-07-12 21:46:55 +0900

    append comment models

--- note/models.py
+++ note/models.py
@@ -32,4 +32,16 @@ class Post(models.Model):
     
     def __str__(self):
         return self.title
+
+class Comment(models.Model):
+    content = models.TextField(max_length=500, blank=False)
+    dt_created = models.DateTimeField(auto_now_add=True)
+    dt_update = models.DateTimeField(auto_now=True)
+    
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return self.content[:30]
     
