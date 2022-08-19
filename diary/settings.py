95ee634ba0a616f22a84e9b475e478299ca80788
woong <dndvud123@naver.com>
2022-08-19 22:27:40 +0900

    interlock mysql

--- diary/settings.py
+++ diary/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+from password import sql_password
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -84,8 +85,12 @@ WSGI_APPLICATION = 'diary.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'dry',
+        'USER': 'root',
+        'PASSWORD': sql_password,
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
